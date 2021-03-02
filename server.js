5ffc33d8b96a929c60bb5895c45befeed3018ff6
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-01 19:47:51 -0500

    working api routes and mysql. Need to add orm, handlebars, and mvc

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+// const exphbs = require('express-handlebars')
 
 const db = require('./connection/connection')('burger','rootroot')
 
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars');
+app.use( express.static('html') );
+
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+// app.set('view engine', 'handlebars');
 
 
+app.get('/api/burgers', async (req,res) =>{
+    const data = await db.query('SELECT * FROM request')
+    res.send(data)
+})
 
+app.post('/api/burgers', async (req,res) => {
+    await db.query('INSERT INTO request (request) VALUES (?)', [req.body.request])
+    res.redirect('/')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.delete('/api/burgers/:id', async ( req,res ) => {
+    const id = req.params.id
+    await db.query(`UPDATE request SET eaten = '1' WHERE id = '${id}'`)
+    res.send({message: `munched ${id}`})
+})
 
 app.listen( PORT , () =>
     console.log(`Server listening on ${PORT}`)
